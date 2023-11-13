3ed675fc3fa4acb50dc968fbc4077e15819d617c
jabuile tovar <jabuiletovar@gmail.com>
2023-11-12 23:16:45 -0300

    metodocreating delete method

--- app.js
+++ app.js
@@ -5,21 +5,32 @@ app.use(express.json());
 let port = 8080;
 
 // arreglo vacio para almacenar
-const task=[];
+const task=[1,2,3,4];
 
 //ruta/metodo para mostrar tareas
 app.get('/tasks', (req, res) => {
-    res.json(task)
+    res.send(task)
     
 })
 
 //ruta/metodo para agregar tarea 
-app.post('/tasks', (req, res) => {
+app.post('/tasks/:nt', (req, res) => {
     const newtask =req.body;
     task.push(newtask);
     res.json(newtask);
 
 });
 
+app.delete('/tasks/:id', (req,res) => {
+   const taskId = req.params.id;
+   const indextask =task.findIndex(p=> p.id === taskId);
+   if (indextask===-1) {
+    res.send("task not found ")
+   } else {
+    task.splice(indextask, 1);
+   }
+
+
+});
 
 app.listen(port,()=>console.log("server up en http://localhost:" + port));
